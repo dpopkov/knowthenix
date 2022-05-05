e604fd4e1d5219e1a095f0c6f28969b1a789af42
Denis Popkov <pkvdenis@gmail.com>
2022-05-06 00:16:37 +0300

    Add Spring Boot project.

---
+++ src/main/java/io/dpopkov/knowthenix/KnowthenixApplication.java
@@ -0,0 +1,13 @@
+package io.dpopkov.knowthenix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowthenixApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowthenixApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/dpopkov/knowthenix/KnowthenixApplicationTests.java
@@ -0,0 +1,13 @@
+package io.dpopkov.knowthenix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowthenixApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
