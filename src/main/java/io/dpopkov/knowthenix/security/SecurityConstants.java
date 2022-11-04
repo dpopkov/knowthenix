db73806c22846528747aeef08005dbf1b2529029
Denis Popkov <pkvdenis@gmail.com>
2022-11-04 18:09:25 +0300

    Security-2: add constants and messages.

---
+++ src/main/java/io/dpopkov/knowthenix/security/SecurityConstants.java
@@ -0,0 +1,24 @@
+package io.dpopkov.knowthenix.security;
+
+public class SecurityConstants {
+
+    /* Time Constants */
+    private static final long MILLIS_PER_DAY = 24 * 60 * 60 * 1000;
+    public static final long EXPIRATION_TIME = 5 * MILLIS_PER_DAY; // 5 days
+
+    /* Authentication Constants */
+    /** This custom header is used for sending generated JWT token when processing login request. */
+    public static final String JWT_HEADER = "Jwt-Token";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String AUTHORITIES = "authorities";
+
+    /* URL Constants */
+    public static final String H2_CONSOLE_URL = "/h2-console/**";
+    private static final String USER_PREFIX = "/user";
+    public static final String[] PUBLIC_URLS = {
+            USER_PREFIX + "/login",
+            USER_PREFIX + "/register",
+            USER_PREFIX + "/resetPassword/**",
+            USER_PREFIX + "/image/**",
+            H2_CONSOLE_URL};
+}
---
+++ src/main/java/io/dpopkov/knowthenix/security/SecurityMessages.java
@@ -0,0 +1,11 @@
+package io.dpopkov.knowthenix.security;
+
+public class SecurityMessages {
+    public static final String COMPANY_NAME = "Knowthenix";
+    public static final String COMPANY_ADMINISTRATION = "Knowthenix Portal";
+    public static final String TOKEN_CANNOT_BE_VERIFIED = "Token Cannot be verified";
+    public static final String FORBIDDEN_MESSAGE = "You do not have permission to access this page";
+    public static final String ACCESS_DENIED_MESSAGE = "You need to log in to access this page";
+    public static final String USER_NOT_FOUND_BY_USERNAME = "User not found by username";
+    public static final String USER_NOT_FOUND_BY_EMAIL = "User not found by email";
+}
