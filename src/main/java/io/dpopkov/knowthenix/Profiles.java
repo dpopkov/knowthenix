71c11477755e3f38819f43326fd5667323b4b39f
Denis Popkov <pkvdenis@gmail.com>
2022-11-20 17:44:32 +0300

    Security-2: add profile "init".

--- src/main/java/io/dpopkov/knowthenix/Profiles.java
+++ src/main/java/io/dpopkov/knowthenix/Profiles.java
@@ -2,5 +2,6 @@ package io.dpopkov.knowthenix;
 
 public class Profiles {
     public static final String DEV = "dev";
+    public static final String INIT = "init";
     public static final String PROD = "prod";
 }
